4e1a0c4ce17952bc7b78ccbe032cf55d74147034
jorgeprietoarranz <jorgeprietoarranz@gmail.com>
2018-09-27 14:44:06 +0100

    Update bayesian.f90

--- src/bayesian.f90
+++ src/bayesian.f90
@@ -1,15 +1,18 @@
+!------------------------------------------------------------
+! This subroutine...
+!------------------------------------------------------------
 subroutine get_loglike(x_rv,y_rv,x_tr,y_tr,e_rv,e_tr, &
            tlab,jrvlab,tff,flags,&
            t_cad,n_cad,pars,rvs,ldc,trends,jrv,jtr, &
            loglike,chi2_rv,chi2_tr,npl,n_tel,n_jrv,size_rv,size_tr)
 implicit none
 
-!In/Out variables
+! In/Out variables
   integer, intent(in) :: size_rv, size_tr, npl, n_tel,n_jrv,n_cad !size of RV and LC data
   double precision, intent(in), dimension(0:size_rv-1) :: x_rv, y_rv, e_rv
   double precision, intent(in), dimension(0:size_tr-1) :: x_tr, y_tr, e_tr
   integer, intent(in), dimension(0:size_rv-1) :: tlab, jrvlab
-!pars = T0, P, e, w, b, a/R*, Rp/R*, K -> for each planet
+! pars = T0, P, e, w, b, a/R*, Rp/R*, K -> for each planet
   double precision, intent(in) :: pars(0:8*npl-1), rvs(0:n_tel-1), ldc(0:1)
   double precision, intent(in) :: t_cad
   double precision, intent(in) :: trends(0:1)
@@ -18,21 +21,21 @@ implicit none
   logical, intent(in) :: flags(0:5)
   logical, intent(in) :: tff(0:1) !total_fit_flag
   double precision, intent(out) :: loglike, chi2_rv, chi2_tr
-!Local variables
+! Local variables
   double precision :: chi2_total
   double precision :: log_errs
   double precision :: two_pi = 2.d0*3.1415926535897932384626d0
   integer :: m
   external:: get_total_chi2
 
-  !Calcualte the chi2
+  ! Calculate the chi2
   call get_total_chi2(x_rv,y_rv,x_tr,y_tr,e_rv,e_tr, &
            tlab,jrvlab,tff,flags,&
            t_cad,n_cad,pars,rvs,ldc,trends,jrv,jtr, &
            chi2_rv,chi2_tr,npl,n_tel,n_jrv,size_rv,size_tr)
 
 
-  !Calculate the normalization term
+  ! Calculate the normalization term
     log_errs = 0.0
     if ( tff(0)  .and. size_rv > 1) then
       do m = 0, size_rv - 1
@@ -57,35 +60,39 @@ implicit none
 end subroutine
 
 
+!------------------------------------------------------------
+! This subroutine...
+!------------------------------------------------------------
 subroutine get_total_chi2(x_rv,y_rv,x_tr,y_tr,e_rv,e_tr, &
            tlab,jrvlab,tff,flags,&
            t_cad,n_cad,pars,rvs,ldc,trends,jrv,jtr, &
            chi2_rv,chi2_tr,npl,n_tel,n_jrv,size_rv,size_tr)
 implicit none
 
-!In/Out variables
+! In/Out variables
   integer, intent(in) :: size_rv, size_tr, npl, n_tel,n_jrv,n_cad !size of RV and LC data
   double precision, intent(in), dimension(0:size_rv-1) :: x_rv, y_rv, e_rv
   double precision, intent(in), dimension(0:size_tr-1) :: x_tr, y_tr, e_tr
   integer, intent(in), dimension(0:size_rv-1) :: tlab, jrvlab
-!pars = T0, P, e, w, b, a/R*, Rp/R*, K -> for each planet
+! pars = T0, P, e, w, b, a/R*, Rp/R*, K -> for each planet
   double precision, intent(in) :: pars(0:8*npl-1), rvs(0:n_tel-1), ldc(0:1)
   double precision, intent(in) :: t_cad
   double precision, intent(in) :: trends(0:1)
   double precision, dimension(0:n_jrv-1), intent(in) :: jrv
   double precision, intent(in) :: jtr
+! flags = is_log_P, is_ew, is_b_factor, is_den_a, is_log_k, is_log_rv0
   logical, intent(in) :: flags(0:5)
   logical, intent(in) :: tff(0:1) !total_fit_flag
   double precision, intent(out) :: chi2_rv, chi2_tr
-!Local variables
+! Local variables
   double precision :: pars_rv(0:7+n_tel-1,0:npl-1)
   double precision :: pars_tr(0:6,0:npl-1)
   logical :: flag_rv(0:3), flag_tr(0:3)
   integer :: i, j
 
-  !Create the parameter variables for rv and tr
+  ! Create the parameter variables for rv and tr
   do i = 0, npl - 1
-    j = 8*i !the new data start point
+    j = 8*i ! the new data start point
     pars_tr(:,i)   = pars(j:j+6)
     pars_rv(0:3,i) = pars(j:j+3)
     pars_rv(4,i) = pars(j+7)
@@ -93,12 +100,12 @@ implicit none
     pars_rv(7:7+n_tel-1,i) = rvs(:)
   end do
 
-  !Put the correct flags
+  ! Put the correct flags
   flag_tr(:)   = flags(0:3)
   flag_rv(0:1) = flags(0:1)
   flag_rv(2:3) = flags(4:5)
 
-  !Let us calculate chi2
+  ! Let us calculate chi2
   chi2_rv = 0.d0
   chi2_tr = 0.d0
 
@@ -109,9 +116,12 @@ implicit none
   call find_chi2_rv(x_rv,y_rv,e_rv,tlab,jrvlab,pars_rv,jrv,&
                     flag_rv,chi2_rv,size_rv,n_tel,n_jrv,npl)
 
-
 end subroutine
 
+
+!------------------------------------------------------------
+! This subroutine...
+!------------------------------------------------------------
 subroutine gauss_prior(mu,sigma,x,prob)
 implicit none
 
@@ -129,18 +139,9 @@ implicit none
 end subroutine
 
 
-subroutine uniform_prior(lx,rx,x,prob)
-implicit none
-
-  !In/Out variables
-  double precision, intent(in) :: lx, rx, x
-  double precision, intent(out)  :: prob
-
-  prob = 1.d0/abs(rx - lx)
-  if ( x < lx .or. x > rx ) prob = 0.d0
-
-end subroutine
-
+!------------------------------------------------------------
+! This subroutine...
+!------------------------------------------------------------
 subroutine get_priors(fit_pars,lims,pars_in,priors_out,npars)
 implicit none
 
@@ -155,8 +156,13 @@ implicit none
   priors_out = 1.d0
   do j = 0, npars - 1
     if ( fit_pars(j) == 'u' ) then
-      call uniform_prior(lims(2*j),lims(2*j+1),pars_in(j),priors_out(j))
-      !end if
+      if ( pars_in(j) < lims(2*j) .or. pars_in(j) > lims(2*j+1) ) then
+        priors_out(j) = 0.d0
+        exit
+      !else
+      !  priors_out(j) = 1 / (lims(2*j+1) - lims(2*j))
+      !  exit
+      end if
     else if ( fit_pars(j) == 'g' ) then
       call gauss_prior(lims(2*j),lims(2*j+1),pars_in(j),priors_out(j))
     end if
